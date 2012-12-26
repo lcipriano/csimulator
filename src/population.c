bc304f2f472ced637293ce69dea4799efa360964
lcipriano <lcipriano@localhost>
2012-12-26 16:29:29 +0000

    Added the Population Class

---
+++ src/population.c
@@ -0,0 +1,8 @@
+/*
+ * population.c
+ *
+ *  Created on: Dec 26, 2012
+ *      Author: lcipriano
+ */
+
+#include "population.h"
---
+++ src/population.h
@@ -0,0 +1,56 @@
+/*
+ * animal.h
+ *
+ *  Created on: Dec 26, 2012
+ *      Author: lcipriano
+ */
+
+#include "date.h"
+#include "smath.h"
+
+typedef struct {
+
+	int nextID;
+
+	/** início da temporada de gestação */
+	int startBreedTime;
+
+	/** fim da temporada de gestação */
+	int endBreedTime;
+
+	/** idade adulta */
+	int adultTime;
+
+	/** tempo médio de vida */
+	int avgTimeLife;
+
+	/** número médio de ninhadas por temporada */
+	int avgBreedsBySeason;
+
+	/** numero médio de crias por casal */
+	int avgKitsByCouple;
+
+	/** gerador aleatório de ninhadas por mês */
+	PoDistri breedsDistri;
+
+	/** gerador aleatório de tempo de vida */
+	UDistri timeLifeDistri;
+
+	/** gerador aleatório de crias por casal */
+	PoDistri kitsDistri;
+
+} Population;
+
+Population newPopulation(int sbt, int ebt, int at, int atl, int abs, int akc);
+
+int getNextID(Population p);
+
+int getStartBreedTime(Population p);
+
+int getEndBreedTime(Population p);
+
+int getLifeDuration(Population p);
+
+int getBreeds(Population p);
+
+int getKits(Population p);
